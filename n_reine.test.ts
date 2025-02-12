6a212b4aa3a2d48e174fd74890754d354967a4f2
Lhukas <58892260+Lhukas@users.noreply.github.com>
2025-02-12 15:22:41 +0100

    Commit 4 - Ajout de test

--- n_reine.test.ts
+++ n_reine.test.ts
@@ -4,9 +4,14 @@ describe('Validation des entrées', () => {
     test('doit lancer une erreur pour une chaîne de caractères', () => {
         expect(() => solveNQueens("4" as any)).toThrow("Entrée non valide : n doit être un entier positif.");
     });
+
+    test('doit lancer une erreur pour n = 0 et n < 0', () => {
+        expect(() => solveNQueens(0)).toThrow("Entrée non valide : n doit être un entier positif.");
+        expect(() => solveNQueens(-1)).toThrow("Entrée non valide : n doit être un entier positif.");
+    });
 });
 
-// On peut conserver le test dummy si souhaité
+
 describe('Tests de base', () => {
     test('dummy test', () => {
         expect(true).toBe(true);
