9a52386dca3e29526f349607cb70c337bee3e8d1
Lhukas <58892260+Lhukas@users.noreply.github.com>
2025-02-12 15:25:03 +0100

    Commit 5 - Ajout de test

--- n_reine.test.ts
+++ n_reine.test.ts
@@ -5,6 +5,10 @@ describe('Validation des entrées', () => {
         expect(() => solveNQueens("4" as any)).toThrow("Entrée non valide : n doit être un entier positif.");
     });
 
+    test('doit lancer une erreur pour un nombre non entier', () => {
+        expect(() => solveNQueens(4.5)).toThrow("Entrée non valide : n doit être un entier positif.");
+    });
+
     test('doit lancer une erreur pour n = 0 et n < 0', () => {
         expect(() => solveNQueens(0)).toThrow("Entrée non valide : n doit être un entier positif.");
         expect(() => solveNQueens(-1)).toThrow("Entrée non valide : n doit être un entier positif.");
